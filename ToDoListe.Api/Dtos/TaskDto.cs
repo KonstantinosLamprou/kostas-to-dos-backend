749a3500aa5d156a63f597bf1cfc4248fd1639e3
kostas <kostaslamprounk@gmail.com>
2026-01-02 23:53:23 +0100

    Einbauen EF Core

--- ToDoListe.Api/Dtos/TaskDto.cs
+++ ToDoListe.Api/Dtos/TaskDto.cs
@@ -1,11 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDoListe.Api.Dtos;
 
 //Ein DTO ist wie ein Vertrag zwischen dem Client und einem Server, da es ein gemeinsames Abkommen 
 //zueinander teilt, wie es Daten transferiert und handled.  
 
 public record TaskDto(
-    int Id, 
-    string Title, 
+    [Required] int Id, 
+    [Required][StringLength(30)]string Title, 
     bool IsComplete, 
     DateOnly TaskDatum 
 );
---
+++ ToDoListe.Api/Models/Task.cs
@@ -0,0 +1,17 @@
+namespace ToDoListe.Api.Models;
+
+//accessor get 
+//mutator set
+//Es sind auto-completed properties  
+// ASP.NET Core nutzt die Setter (set) der Klasse, um die Daten aus dem JSON als C#-Objekt zu füllen.
+// Wenn man die Daten später wieder als Antwort zurückschickt, nutzt ASP.NET Core die Getter (get), um das JSON zu erzeugen.
+public class Task
+{
+    public required int Id { get; set; }
+    public required string Title { get; set; }
+
+    public bool IsComplete { get; set; }
+
+    public DateOnly TaskDatum { get; set; }
+
+} 
